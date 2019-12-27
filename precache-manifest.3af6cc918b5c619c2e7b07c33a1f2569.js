d532719f914e2a4951984a76874a8ba0198e4988
sbahat1214 <bcs.f15.43@gmail.com>
2019-12-27 20:57:55 +0500

    rooms page fixed

---
+++ precache-manifest.3af6cc918b5c619c2e7b07c33a1f2569.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96907dc9828b7bd3bc5583c11373403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bf6d4859179abee7a86",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d5accdcc5ffa31df2a58",
+    "url": "/static/css/main.55792f82.chunk.css"
+  },
+  {
+    "revision": "7bf6d4859179abee7a86",
+    "url": "/static/js/2.8b5d7bca.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/static/js/2.8b5d7bca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5accdcc5ffa31df2a58",
+    "url": "/static/js/main.b4752fd5.chunk.js"
+  },
+  {
+    "revision": "f4fd2fe2d2780f84f762",
+    "url": "/static/js/runtime-main.ea4cb42b.js"
+  },
+  {
+    "revision": "fd249939f9be10c4d10fb9ca7e92497d",
+    "url": "/static/media/bgImage.fd249939.jpg"
+  }
+]);
